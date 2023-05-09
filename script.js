b41a9794af872f555d4982ae1693bcf049e2d713
lilia <apmk2355@gmail.com>
2023-05-09 09:09:57 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+a = 5;
+console.log(a);
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+console.log(4/0);
+console.log('string'*9);
+
+// let persone = {
+//     name:"Lilia",
+//     age:25,
+//     isMarried: false
+// }
+// console.log(persone["name"]);
+
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
+// console.log(arr[0]);
+
+// alert("Hello world!");
+
+// let anser = confirm("Are you here?");
+// console.log(anser);
+// let anser = +prompt("Есть ли вам 18?", "Да");
+//  console.log(typeof(anser));
+
+//  console.log("arr" + "-object");
+//  console.log(4 +"-object");
+let incr = 10,
+    decr = 10;
+
+    // incr++;
+    // decr--;
+    console.log(++incr);
+    console.log(--decr);
+    console.log(5%2);
+    console.log("2" === 2);
+    let isChecked = false,
+    isClose = false;
+    console.log(isChecked && isClose);
+    console.log(isChecked || !isClose);
+
